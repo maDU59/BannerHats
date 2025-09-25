fff5c134caee4d526806e85a66b5b3015ae345cb
maDU59_ <matthias.ducourant@gmail.com>
2025-09-25 11:17:55 +0200

    Removed useless imports

--- src/client/java/com/madu59/wearable_banners/WearableBannersClient.java
+++ src/client/java/com/madu59/wearable_banners/WearableBannersClient.java
@@ -1,9 +1,6 @@
 package com.madu59.wearable_banners;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.stat.Stats;
 
 public class WearableBannersClient implements ClientModInitializer {
 
